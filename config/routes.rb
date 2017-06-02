4abea2b425f1e89943a502c503a19d0fa8ff4b87
Nicolas Bresson <bresson.nicolas@gmail.com>
2017-06-03 01:01:22 +0200

    queries controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'queries/show'
+
+  get 'queries/new'
+
+  get 'queries/create'
+
+  get 'queries/edit'
+
+  get 'queries/update'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
