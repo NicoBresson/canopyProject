2027593f3702494455eed3fa4a6553a4fe973bf3
Nicolas Bresson <bresson.nicolas@gmail.com>
2017-07-11 12:01:18 -0500

    frontpage banner dummmy

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  get 'graphs/show'
-
-  get 'queries/show'
-
-  get 'queries/new'
-
-  get 'queries/create'
-
-  get 'queries/edit'
-
-  get 'queries/update'
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
